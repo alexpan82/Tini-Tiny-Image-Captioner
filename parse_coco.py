7877d92945b7572f00b60e7f17b5335e7f07001b
alexpan82 <apan82@gmail.com>
2023-04-26 21:00:13 -0400

    Cleaned up tinyvit implementation

--- parse_coco.py
+++ parse_coco.py
@@ -13,10 +13,10 @@ def main(clip_model_type: str):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     clip_model_name = clip_model_type.replace('/', '_')
     out_path = f"./data/coco/oscar_split_{clip_model_name}_train.pkl"
-    clip_model, preprocess = clip.load(clip_model_type, device=device, jit=False)
-
-    print(device)
-    tinyvit_model = Encoder(device).to(device)
+    if clip_model_type == 'tinyvit':
+        tinyvit_model = Encoder(device).to(device)
+    else:
+        clip_model, preprocess = clip.load(clip_model_type, device=device, jit=False)
 
     with open('./data/coco/annotations/train_caption.json', 'r') as f:
         data = json.load(f)
@@ -33,14 +33,14 @@ def main(clip_model_type: str):
         image = preprocess(Image.fromarray(image)).unsqueeze(0).to(device)
         # image.shape = torch.Size([1, 3, 224, 224]) with values [0, 1]
         with torch.no_grad():
-            # prefix = clip_model.encode_image(image).cpu()
-            logits = tinyvit_model.forward(image).cpu()
-            prefix = tinyvit_model.norm_head
-            print(prefix.shape)
-            # prefix.shape = torch.Size([1, 512]) dtype=torch.float16
-            # tinyvit_out.shape = torch.Size([1, 1000]) dtype=torch.float32
-            # TODO: replace clip_model.encode_image(image).cpu() with tinyvit_model.foward(image).cpu()
-            # Add make sure the relevent dims in train.py are = 1000 (since that's the embedding dim of tinyvit)
+            if clip_model_type == 'tinyvit':
+                logits = tinyvit_model.forward(image).cpu()
+                prefix = tinyvit_model.norm_head
+                # tinyvit_out.shape = torch.Size([1, 576]) dtype=torch.float32
+            else:
+                prefix = clip_model.encode_image(image).cpu()
+                # prefix.shape = torch.Size([1, 512]) dtype=torch.float16
+                
         d["clip_embedding"] = i
         all_embeddings.append(prefix)
         all_captions.append(d)
@@ -58,6 +58,6 @@ def main(clip_model_type: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--clip_model_type', default="ViT-B/32", choices=('RN50', 'RN101', 'RN50x4', 'ViT-B/32'))
+    parser.add_argument('--clip_model_type', default="tinyvit", choices=('RN50', 'RN101', 'RN50x4', 'ViT-B/32', 'tinyvit'))
     args = parser.parse_args()
     exit(main(args.clip_model_type))
